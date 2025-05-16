d3360935f9b3df1b80acb7ab0f0bcd646e72bed1
laryTgs <laryssatortato99@gmail.com>
2025-05-16 18:38:47 -0300

    up

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const scrollToTop = () => {
     behavior: "smooth",
   });
 };
+
+window.addEventListener("load", () => {
+  const fill = document.querySelector(".progress-fill");
+  const content = document.getElementById("content");
+  const preloader = document.getElementById("preloader");
+
+  let progress = 0;
+  const interval = setInterval(() => {
+    progress += 1;
+    fill.style.width = progress + "%";
+
+    if (progress >= 100) {
+      clearInterval(interval);
+      setTimeout(() => {
+        preloader.style.display = "none";
+        content.style.display = "block";
+      }, 300); // pequeno delay para suavidade
+    }
+  }, 15); // velocidade da animação
+});
